030d14f034f652e905f1b58c1f553a8811f5bc92
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2022-10-21 21:04:04 +0200

    Tasks Of Day 01
    -HelloWorld!
    -TakeChar&GetAsciCode
    -TakeNumber&GetHexa

---
+++ Introduction to Programming using C/Lab 01/TakeChar&GetAsciCode/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    printf("ASKI: %d\n", c);
+    return 0;
+}
---
+++ Introduction to Programming using C/Lab 01/TakeNumber&GetHexa/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    printf("enter number");
+    scanf("%d",&number);
+    printf("hexa: %x\n",number);
+    return 0;
+}
