9df09314d9785236de141e808e2fde2f67e7db97
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2022-12-14 17:04:11 +0200

    Revision

---
+++ JavaScript/Demo/part 1/scripting.js
@@ -0,0 +1,145 @@
+//document.writeln("Hello World ! <br>");
+// document.write("Wellcome frome Scripting File !");
+
+// PrintFun();
+// function PrintFun()
+// {
+//     document.write("Hello From Function ! <br>");
+// }
+
+// PrintFun();
+
+// function testNumber(x,y)
+// {
+//     if (x>y)
+//     {
+//        document.write("Your Result Is :true <br>");
+//     }
+//     else
+//     {
+//         document.write("Your Result is :false <br>");
+//     }
+// }
+
+// function SumTowNumbers(x,y)
+// {
+//     document.write(x+y);
+// }
+
+// testNumber(5,6);
+// testNumber(10,6);
+
+// var NumOne=parseInt(prompt("Enter Your First Number"));
+// var NumTwo=parseInt(prompt("Enter Your Second Number"));
+// SumTowNumbers(NumOne,NumTwo);
+
+// var x=10 ;
+// var y=5 ;
+
+// function testScope()
+// {
+//     var z=5;
+//     document.write(x+y+z);
+// }
+
+// testScope();               //20
+// document.write(x+y+z);     //20  XXXXXXXXXXXXX--> z is defined in local scope of function 
+
+
+//Errors 
+// 1- Syntax Error
+// 2- logical Error
+//
+// 
+
+
+
+// function sumXZ(x,z)
+// {
+//     return x*z;
+// }
+
+//sumXZ(2,3);
+// console.log(sumXZ(3,4));
+
+// var s=new String ('Name maged');
+// var str="My Name Is Maged";
+
+
+// var y=new Number(22);
+// var x=1234;
+
+
+// var cArea= Math.PI*3*3;
+
+// var arr=new Array(3);
+// arr[0]='red';
+// arr[1]='green';
+// arr[2]='blue';
+// arr[3]='black';
+
+// var arr2=new Array('maged',123,'mohamed',arr);
+
+// var assocarr=new Array();
+// assocarr['maged']="menofia";
+// assocarr['mina']='cairo';
+
+// var mydate=new Date();
+
+//var b=new Boolean(0);
+
+//var myreg=new RegExp();
+
+// var win;
+
+// function OpenMyWindow()
+// {
+//    win= window.open("ChildIndex.html","","width=500 , height=500");
+// }
+
+// function CloseMyWindow()
+// {
+//     win.close();
+// }
+
+// function MoveYourWIndow()
+// {
+//     win.moveTo(600,600);
+//     win.focus();
+// }
+
+// //setInterval(alert("Wellcome to our Page"),1000);
+// var timer;
+// function StartAlert()
+// {
+//   timer= setInterval("alert('wellcome to our page !')",2000);
+// }
+
+// function StopAlert()
+// {
+//     clearInterval(timer);
+// }
+
+//JavaScript Engin
+// console.log('1');
+// fun();
+// setTimeout(() => {
+//     console.log('2')
+// }, 1000);
+// setTimeout(() => {
+//     console.log('3')
+// }, 0);
+// fun();
+// console.log('4');
+
+// function fun()
+// {
+//     console.log('5');
+// }
+
+// function LocationReplace()
+// {
+//     // location.replace('https://www.facebook.com');
+//     //location.reload();
+// }
+
