45c0d5fbd36f39b098be0ed3c43b3998e0abe5f0
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2022-12-15 09:39:33 +0200

    Revision

---
+++ JavaScript/Demo/part 2/scripting.js
@@ -0,0 +1,22 @@
+// document.write("Hello from Scripting File !");
+
+
+//  var Content = document.getElementById("headone")
+//  console.log(Content);
+
+// var Content = document.getElementsByName("parone")[0].innerText
+// console.log(Content)
+
+
+// var Content = document.getElementsByTagName("h2")[1].innerText
+// console.log(Content);
+
+
+// var Content = document.getElementsByClassName("partwo")[0].innerText
+// console.log(Content);
+
+// function writtingMyText(txt)
+// {
+// document.getElementById("myDiv").innerText = txt.value;
+
+// }
