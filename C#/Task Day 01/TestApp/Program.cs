5aec15f0180634d3dd0f1bd519b77722080a4eea
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-01-18 23:14:49 +0200

    Tasks Of Day 01

---
+++ C#/Task Day 01/TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class test
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void SayWelcome()
+        {
+            Console.WriteLine("welcome");
+        }
+    }
+
+}
