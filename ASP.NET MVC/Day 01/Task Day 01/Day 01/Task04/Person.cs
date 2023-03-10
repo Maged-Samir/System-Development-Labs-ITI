9e502220b7732fa670179850cf0d9853ee62fcd3
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-03-10 10:58:27 +0200

    Tasks Of Day 01

---
+++ ASP.NET MVC/Day 01/Task Day 01/Day 01/Task04/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day_01.Task04
+{
+    public class Person
+    {
+         public int Id { get; set; }
+        public string Name { get; set; }
+        public float Salary { get; set; }
+    }
+}
