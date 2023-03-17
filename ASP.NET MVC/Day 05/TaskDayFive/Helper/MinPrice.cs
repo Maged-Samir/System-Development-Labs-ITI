12b6c1cf039f0dc99830a7c03016c3d6215829a4
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-03-17 18:48:41 +0200

    Task Day 05

---
+++ ASP.NET MVC/Day 05/TaskDayFive/Helper/MinPrice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskDayFive.Helper
+{
+    public class MinPrice:ValidationAttribute
+    {
+        int value;
+        public MinPrice(int num)
+        {
+            value = num;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if(obj == null )
+            {
+                return false;
+            }
+            else
+            {
+                if(obj is int )
+                {
+                    int suppliedvalue = (int)obj;
+                    if(suppliedvalue > value)
+                    { 
+                    return true;
+                    }
+                    else
+                    {
+                        ErrorMessage = " Minmum Value For Price Shoul be +" + value;
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ASP.NET MVC/Day 05/TaskDayFive/Migrations/202303091108039_CreateDataBase.cs
@@ -0,0 +1,46 @@
+﻿namespace TaskDayFive.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDataBase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CustomerInfo",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 30),
+                        gender = c.Int(nullable: false),
+                        Email = c.String(nullable: false),
+                        ConfirmEmail = c.String(nullable: false),
+                        PhoneNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.OrderInfo",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        TotalPrice = c.Int(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.CustomerInfo", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderInfo", "CustomerId", "dbo.CustomerInfo");
+            DropIndex("dbo.OrderInfo", new[] { "CustomerId" });
+            DropTable("dbo.OrderInfo");
+            DropTable("dbo.CustomerInfo");
+        }
+    }
+}
