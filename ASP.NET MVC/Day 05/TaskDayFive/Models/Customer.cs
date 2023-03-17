7225d2f937f8e0f5e1468d5727d13d520bdf82c8
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-03-17 18:50:18 +0200

    Tasks Of Day 05

---
+++ ASP.NET MVC/Day 05/TaskDayFive/Models/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TaskDayFive.Models
+{
+    public enum Gender { Male, Female }
+
+
+    [Table("CustomerInfo")]
+    public class Customer
+    {
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        [MaxLength(30, ErrorMessage = "Max length of Your Name 30 Char")]
+        [Display(Name = "CustomerName")]
+        public string Name { get; set; }
+
+
+
+        [Required]
+        [EnumDataType(typeof(Gender))]
+        public Gender gender { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Email ")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Re-Enter Your Mail")]
+        [Compare("Email", ErrorMessage = "Email Not match!!!")]
+        public string ConfirmEmail { get; set; }
+
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        
+        public virtual List<Order> Orders { get; set; }
+    }
+}
---
+++ ASP.NET MVC/Day 05/TaskDayFive/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using TaskDayFive.Helper;
+
+namespace TaskDayFive.Models
+{
+
+    [Table("OrderInfo")]
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+
+
+        [Required, DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime OrderDate { get; set; }
+
+        [MinPrice(20)]
+        public int TotalPrice { get; set; }
+
+        
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
