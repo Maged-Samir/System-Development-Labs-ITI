69e202032e6786de0806f3b08ecf2b2d9932228e
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-05-01 15:25:52 +0300

    Tasks Of Day 01

---
+++ MVVM/MVVM.Task.Day01/MVVM.Task.Day01/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Task.Day01.Models
+{
+    public class User
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string FullName => $"{Name} - {Age} Years";
+    }
+}
---
+++ MVVM/MVVM.Task.Day01/MVVM.Task.Day01/ViewModels/UserViewModel.cs
@@ -0,0 +1,104 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MVVM.Task.Day01.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MVVM.Task.Day01.ViewModels
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private User _user;
+        private ObservableCollection<User> _users;
+        private User _selectedUser;
+
+        public UserViewModel()
+        {
+            _user = new User();
+            _users = new ObservableCollection<User>();
+            AddUserCommand = new RelayCommand(AddOrUpdateUser);
+        }
+
+        public string Name
+        {
+            get { return _user.Name; }
+            set
+            {
+                _user.Name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public int Age
+        {
+            get { return _user.Age; }
+            set
+            {
+                _user.Age = value;
+                RaisePropertyChanged("Age");
+            }
+        }
+
+        public ObservableCollection<User> Users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                RaisePropertyChanged("Users");
+            }
+        }
+
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                if (_selectedUser != null)
+                {
+                    Name = _selectedUser.Name;
+                    Age = _selectedUser.Age;
+
+                    RaisePropertyChanged("Users");
+
+                }
+                else
+                {
+                    Name = string.Empty;
+                    Age = 0;
+                }
+                RaisePropertyChanged("SelectedUser");
+
+            }
+        }
+
+        public ICommand AddUserCommand { get; private set; }
+
+        private void AddOrUpdateUser()
+        {
+            if (SelectedUser != null)
+            {
+                SelectedUser.Name = Name;
+                SelectedUser.Age = Age;
+                RaisePropertyChanged("Users");
+            }
+            else
+            {
+                Users.Add(new User { Name = Name, Age = Age });
+            }
+            Name = string.Empty;
+            Age = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
