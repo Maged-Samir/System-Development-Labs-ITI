123078decba28b808b9a5342198b2c9a6165d821
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-06-20 18:58:22 +0300

    -LINQ

---
+++ LINQ/ITI.Revision.LINQ/ITI.Revision.LINQ/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Revision.LINQ
+{
+    internal class Employee:IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(int Id,string Name,int Age,int Salary)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.Salary = Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}-{Name}  , {Age} Years Old - {Salary}$";
+        }
+
+        public int CompareTo(Employee? other)
+        => Salary.CompareTo(other.Salary);
+
+        public static List<Employee> empList { get; set; } = new List<Employee>()
+        {
+            new Employee(1,"Ahmed",20,1500),
+            new Employee(3,"Mohamed",22,2000),
+            new Employee(5,"Amany",21,1800),
+            new Employee(2,"Sara",23,1900),
+            new Employee(4,"Omnia",24,1800)
+        };
+    }
+}
---
+++ LINQ/ITI.Revision.LINQ/ITI.Revision.LINQ/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace ITI.Revision.LINQ
+{
+    public static class IntExtensions
+    {
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Implicitly Typed Local Variable
+            //int x = 5;
+            //x = "ahmed";
+
+            //double y = 10.5;
+            //y = "ahmed";
+
+            //var x = 5;
+            //var y = null;
+
+            #endregion
+
+            #region Extention Method
+            //Extension methods allow you to inject additional methods without modifying,
+            //deriving or recompiling the original class
+
+            //int i = 10;
+            //bool result = i.IsGreaterThan(100);
+            //Console.WriteLine(result);
+
+            #endregion
+
+            #region Anonymous Type
+            //var emp1 = new { id = 2, name = "ahmed", age = 20 };
+            //Console.WriteLine(emp1.GetHashCode());
+            //Console.WriteLine(emp1.ToString());
+
+            //var emp2 = new { id = 2, name = "ahmed", age = 20 };
+            //Console.WriteLine(emp2.GetHashCode());
+
+
+            #endregion
+
+            #region Shapes Of Writing Linq Quries
+
+            //List<int> list = new(){ 1,2,3,5,4,6,2};
+
+            //1-Static method , static class
+            //var Result=Enumerable.Where(list, i => i > 4);
+
+            //2-Extention Method - Flunent Syntax
+            //var Result = list.Where(x => x > 3);
+
+            //3-Query Synatax
+            //var Result = from i in list
+            //             where i > 3
+            //             select i;
+
+            //foreach(var item in Result) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Linq 
+            //var Result = Employee.empList.Where(x => x.Age>22);
+            //var Result = Employee.empList.Where(x => x.Salary > 1500).Select(x => x.Name);
+            //var Result = Employee.empList.Select(x => x.ToString());
+            //var Result = Employee.empList.Select(x => new { x.Name, x.Age });
+            //var Result = Employee.empList.Where(x => x.Age>20).OrderBy(x=>x.Salary);
+
+
+
+            //foreach(var item  in Result) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Single /Last 
+            //var Result = Employee.empList.First();
+            //var Result = Employee.empList.Last();
+
+            //var Result = Employee.empList.Where(x => x.Age > 50).FirstOrDefault();
+            //var Result = Employee.empList.FirstOrDefault(e => e.Age > 20);
+
+            //var Result = Employee.empList.Single();
+            //var Result = Employee.empList.Single(x=>x.Age==22);
+
+            //var Result = Employee.empList.Count();
+            //var Result = Employee.empList.Max();  //need  IComparable
+            //var Result = Employee.empList.Min();
+
+            //Console.WriteLine(Result);
+
+            //var Result = Employee.empList.Take(2);
+            //var Result = Employee.empList.Skip(2);
+            //var Result = Employee.empList.TakeLast(2);
+            //var Result = Employee.empList.SkipLast(2);  
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
