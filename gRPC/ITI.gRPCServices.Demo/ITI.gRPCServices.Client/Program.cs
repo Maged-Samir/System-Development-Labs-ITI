55317b8cf8a6bb553d005a07ee65ffae0904f367
Maged Samir <102479398+Maged-Samir@users.noreply.github.com>
2023-07-07 19:02:26 +0300

    - Demo

---
+++ gRPC/ITI.gRPCServices.Demo/ITI.gRPCServices.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using ITI.gRPCServices.Server;
+using ITI.gRPCServices.Server.Protos;
+
+namespace ITI.gRPCServices.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var greeterChannel = GrpcChannel.ForAddress("https://localhost:7191");
+            var greeterClient = new Greeter.GreeterClient(greeterChannel);
+
+            Console.WriteLine("Please Enter Your Name:");
+            string name = Console.ReadLine();
+
+            var greeterResponse = await greeterClient.SayHelloAsync(new HelloRequest { Name = name });
+            Console.WriteLine("Greeter Response: " + greeterResponse.Message);
+
+            var timeChannel = GrpcChannel.ForAddress("https://localhost:7191");
+            var timeServiceClient = new TimeService.TimeServiceClient(timeChannel);
+
+            var timeResponse = await timeServiceClient.GetCurrentTimeAsync(new ITI.gRPCServices.Server.Protos.Empty());
+            Console.WriteLine("Current Time: " + timeResponse.CurrentTime);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPC/ITI.gRPCServices.Demo/ITI.gRPCServices.Server/Services/TimeServiceGrpc.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using ITI.gRPCServices.Server.Protos;
+
+namespace ITI.gRPCServices.Server.Services
+{
+    public class TimeServiceGrpc : TimeService.TimeServiceBase
+    {
+        public override Task<TimeResponse> GetCurrentTime(Empty request, ServerCallContext context)
+        {
+            string currentTime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+            return Task.FromResult(new TimeResponse { CurrentTime = currentTime });
+        }
+    }
+}
